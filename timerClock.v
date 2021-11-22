1ce3627c68b92c45b24ed49abaa36300e93d5b41
rajivramroop <75186655+rajivramroop@users.noreply.github.com>
2021-11-22 18:41:15 -0500

    Create timerClock.v

---
+++ timerClock.v
@@ -0,0 +1,19 @@
+module timeClock (
+input clk,
+output reg clk_out);
+
+reg[37:0] period_count=0;
+
+always @(posedge clk) //1Hz Clock
+  if (period_count != 100,000,000 -1) //100Mhz
+  begin 
+  period_count <= period count +1;
+    clk_out <=0;
+    end 
+   
+  else 
+      begin 
+      period_count < = 0;
+      clk_out <1;
+ end 
+ endmodule 
