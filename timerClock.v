3032a3b6dbfb32253ba196fb7e38e1500a463c33
rajivramroop <75186655+rajivramroop@users.noreply.github.com>
2021-11-22 20:25:07 -0500

    Update timerClock.v

--- timerClock.v
+++ timerClock.v
@@ -2,7 +2,7 @@ module timeClock (
 input clk,
 output reg clk_out);
 
-reg[37:0] period_count=0;
+  reg[27:0] period_count=0;
 
 always @(posedge clk) //1Hz Clock
   if (period_count != 100,000,000 -1) //100Mhz
